52fad7aceaf7e5ffa930326626fb4fb8aa638425
Mariana <nanomarii10@gmail.com>
2023-04-19 13:36:10 -0300

    feat: finalized features and correction of visual details

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const form = document.querySelector("#form-habits");
+const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const buttonAdd = document.querySelector('header button');
 
-const data = {
-  exercise: ["01-01", "01-02", "01-03", "01-04", "01-07", "01-08", "01-09"],
-}
+buttonAdd.addEventListener('click', addDay);
+form.addEventListener('change', saveChange)
 
-nlwSetup.setData(data);
-nlwSetup.load();
+function addDay() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert('Esse dia já está incluso!');
+    return
+  }
+  nlwSetup.addDay(today);
+  alert('Dia adicionado com sucesso!')
+};
+
+function saveChange() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
+};
+
+ const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
+ nlwSetup.setData(data);
+ nlwSetup.load();
+  
